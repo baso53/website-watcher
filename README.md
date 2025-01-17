eae0dab224ecbf8b698f04a8d04f552859d1f740
Sebastijan Grabar <8758580+baso53@users.noreply.github.com>
2025-01-17 19:30:32 +0100

    Add script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 A simple bash script that loads a website and pushes a message with [Telepush](https://github.com/muety/telepush) when it detects that the specific string isn't present on the website. (for example, to see if a product isn't sold out anymore)
 
+# How to use
+
+1. Read the [Telepush README](https://github.com/muety/telepush?tab=readme-ov-file#%EF%B8%8F-how-to-use) on how to get a token.
+2. Replace the variables in `website-watcher.sh` with the website and string you want to match and paste your Telepush token.
+3. Run `website-watcher.sh`
---
+++ website-watcher.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Define variables
+URL="https://www.example.com"  # Replace with the website URL you want to check
+STRING_TO_FIND="String you want to find"  # Replace with the string you want to search for
+TELEPUSH_TOKEN="your-api-token"  # Replace with your actual Telepush API token
+TELEPUSH_ENDPOINT="https://telepush.dev/api/messages/${TELEPUSH_TOKEN}"
+
+# Download the website content
+response=$(curl -s "$URL")
+
+# Check if the string is not found in the response
+if [[ "$response" != *"$STRING_TO_FIND"* ]]; then
+    # If the string is not found, send a notification via Telepush
+    curl -X POST "$TELEPUSH_ENDPOINT" \
+    -H "Content-Type: application/json" \
+    -d '{
+            "text": "Alert: The string '"$STRING_TO_FIND"' was not found on '"$URL"'"
+        }'
+    echo "Notification sent."
+else
+    echo "String found."
+fi
