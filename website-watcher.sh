723f943098fec68ad0ac6a5cbeb2d7c8989a461c
Sebastijan Grabar <8758580+baso53@users.noreply.github.com>
2025-01-17 19:36:47 +0100

    Add alert message as a variable

--- website-watcher.sh
+++ website-watcher.sh
@@ -4,6 +4,7 @@
 URL="https://www.example.com"  # Replace with the website URL you want to check
 STRING_TO_FIND="String you want to find"  # Replace with the string you want to search for
 TELEPUSH_TOKEN="your-api-token"  # Replace with your actual Telepush API token
+ALERT_MESSAGE="Alert: The string '${STRING_TO_FIND}' was not found on '${URL}'." # Customize the alert message
 TELEPUSH_ENDPOINT="https://telepush.dev/api/messages/${TELEPUSH_TOKEN}"
 
 # Download the website content
@@ -15,7 +16,7 @@ if [[ "$response" != *"$STRING_TO_FIND"* ]]; then
     curl -X POST "$TELEPUSH_ENDPOINT" \
     -H "Content-Type: application/json" \
     -d '{
-            "text": "Alert: The string '"$STRING_TO_FIND"' was not found on '"$URL"'"
+            "text": "'"${ALERT_MESSAGE}"'"
         }'
     echo "Notification sent."
 else
